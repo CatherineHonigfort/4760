43454335edc694585309e5393b53808ac7690f27
CatherineHonigfort <chonigfo@slu.edu>
2017-05-15 11:48:51 -0500

    Update logs.c

--- logs.c
+++ logs.c
@@ -1,3 +1,4 @@
+//Just in case you check my github before you see my email, I just wanted to let you know that as soon as I get home I will load these up on the hoare server. (I'm updating the github instead of just uploading them now because the compouter with the files on it is on the other side of campus, but I had time to sprint to the lab and update this before my finals) I'm so sorry, I didn't see your email, and didn't realize that I had access to the right server so I tested my projects on my SLU server (hopper.slu.edu) and submitted them on github. I'm sorry!
 #include <time.h>
 #include "logs.h"
 #include <string.h>
