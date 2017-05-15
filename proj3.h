bfa0dfab411618bca9238add0496d1a9088e4eb7
CatherineHonigfort <chonigfo@slu.edu>
2017-05-15 11:51:14 -0500

    Update proj3.h

--- proj3.h
+++ proj3.h
@@ -1,3 +1,4 @@
+//Just in case you check my github before you see my email, I just wanted to let you know that as soon as I get home I will load these up on the hoare server. (I'm updating the github instead of just uploading them now because the compouter with the files on it is on the other side of campus, but I had time to sprint to the lab and update this before my finals) I'm so sorry, I didn't see your email, and didn't realize that I had access to the right server so I tested my projects on my SLU server (hopper.slu.edu) and submitted them on github. I'm sorry!
 #pragma once
 #define SHM_KEY 9823
 #define MESSAGE_QUEUE_KEY 3318
